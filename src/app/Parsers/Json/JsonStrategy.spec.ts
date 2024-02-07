3b759693e27e69c427bbf72152e5d760f413a8b6
Jason Fu <r.fu1@student.tue.nl>
2024-02-07 13:20:48 +0100

    create unit tests for JsonReading

---
+++ src/app/Parsers/Json/JsonStrategy.spec.ts
@@ -0,0 +1,41 @@
+import {JsonReadingStrategy} from "./JsonStrategy";
+
+it('should read a basic JSON correctly', () => {
+    let json = new JsonReadingStrategy();
+    let object = json.read('{"a": 1, "b": 2}');
+    expect(object).toEqual({a: 1, b: 2});
+})
+
+it('should throw an error when reading an invalid JSON', () => {
+    let json = new JsonReadingStrategy();
+    expect(() => json.read('{"a": 1, "b": 2')).toThrowError(SyntaxError);
+})
+
+it('should throw an error when reading a number but invalid JSON', () => {
+    let json = new JsonReadingStrategy();
+    expect(() => json.read('1')).toThrowError(SyntaxError);
+})
+
+it('should throw an error when reading a string but invalid JSON', () => {
+    let json = new JsonReadingStrategy();
+    expect(() => json.read('"1"')).toThrowError(SyntaxError);
+})
+
+it('should throw an error when reading a boolean but invalid JSON', () => {
+    let json = new JsonReadingStrategy();
+    expect(() => json.read('true')).toThrowError(SyntaxError);
+})
+
+it('should throw an error when reading a null but invalid JSON', () => {
+    let json = new JsonReadingStrategy();
+    expect(() => json.read('null')).toThrowError(SyntaxError);
+})
+it('should throw an error when reading empty but invalid JSON', () => {
+    let json = new JsonReadingStrategy();
+    expect(() => json.read('')).toThrowError(SyntaxError);
+})
+
+it('should throw an error when reading an empty string but invalid JSON', () => {
+    let json = new JsonReadingStrategy();
+    expect(() => json.read('""')).toThrowError(SyntaxError);
+})
